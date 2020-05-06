61ed3837ba6aafa1864b1e5311499e22a859e4dd
Luiz <luiz_higuti@hotmail.com>
2020-05-06 19:23:08 -0300

    Changes to be committed: 	new file:   assets/Cidades-SP.txt 	modified:   lib/main.dart 	modified:   pubspec.lock 	modified:   pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 // Copyright 2018 The Flutter team. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+import 'dart:async' show Future;
 
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 void main() => runApp(MyApp());
 
@@ -11,47 +12,59 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Start Up Name Generator',
+      title: 'City Name Generator',
       home: RandomWords(),
     );
   }
 }
 
 class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
+  final _suggestions = <String>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
+  var fileContents = <String>[];
+  
   @override
   Widget build(BuildContext context) {
+    loadAsset().then((contents) {
+      setState(() {
+      });
+        fileContents = contents.split("\n");
+    });
     return Scaffold(
       appBar: AppBar(
-        title: Text('Startup Name Generator'),
+        title: Text('City Name Generator'),
       ),
       body: _buildSuggestions(),
     );
   }
 
-  Widget _buildRow(WordPair pair) {
-  return ListTile(
-    title: Text(
-      pair.asPascalCase,
-      style: _biggerFont,
-    ),
-  );
-}
+  Widget _buildRow(String string) {
+    return ListTile(
+      title: Text(
+        string,
+        style: _biggerFont,
+      ),
+    );
+  }
 
   Widget _buildSuggestions() {
-  return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: /*1*/ (context, i) {
-        if (i.isOdd) return Divider(); /*2*/
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return Divider(); /*2*/
 
-        final index = i ~/ 2; /*3*/
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-        }
-        return _buildRow(_suggestions[index]);
-      });
-}
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            _suggestions.add(fileContents[index]); /*4*/
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Future<String> loadAsset() async {
+    String fileContents = await rootBundle.loadString('assets/Cidades-SP.txt');
+    return fileContents;
+  }
 }
 
 class RandomWords extends StatefulWidget {
